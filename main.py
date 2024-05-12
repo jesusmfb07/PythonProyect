b323b2690fcf4ec320834756e03ceb564a329f51
Jesus Mendoza <jesusmfb07@gmail.com>
2024-05-12 12:49:05 -0500

    correction 4

--- main.py
+++ main.py
@@ -3,65 +3,116 @@ import json
 import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from lxml import etree
-from io import BytesIO
+import feedparser
+from bs4 import BeautifulSoup
+import re
+
+def check_rss_fetchrss_not_found(response_text):
+    if "Feed does not exists" in response_text:
+        return "RSS_FETCHRSS_NOT_FOUND"
+    else:
+        return None
+
+def check_rss_rssapp_not_found(response_text):
+    if "RSS.app - Feed Generator" in response_text:
+        return "RSS_RSSAPP_NOT_FOUND"
+    else:
+        return None
+
+def check_response_status(response):
+    status_code = response.status_code
+    error_type = None
+
+    if status_code == 404:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title_tags = soup.find_all('title')
+        for title_tag in title_tags:
+            title_text = title_tag.get_text()
+            if "Feed does not exists" in title_text:
+                error_type = "RSS_FETCHRSS_NOT_FOUND"
+                break
+            elif "RSS.app - Feed Generator" in response.text:
+                error_type = "RSS_RSSAPP_NOT_FOUND"
+                break
+
+        if not error_type:
+            error_type = "ERROR_404"
+
+    elif status_code == 500:
+        error_type = "ERROR_500"
+    elif status_code == 403:
+        error_type = "ERROR_403"
+    elif status_code == 400:
+        error_type = "ERROR_400"
+    elif status_code == 402:
+        error_type = "ERROR_402"
+
+    return error_type, status_code
+
+def analyze_content(response):
+    try:
+        content_type = response.headers.get("Content-Type", "").lower()
+        content_text = response.text.strip()
+
+        # Verificar si el contenido parece ser un archivo XML válido
+        if content_text.startswith('<?xml'):
+            soup = BeautifulSoup(content_text, 'xml')
+            rss = soup.find('rss')
+            if rss:
+                channel = rss.find('channel')
+                if channel:
+                    items = channel.find_all('item')
+                    num_items = len(items)
+                    if num_items > 0:
+                        # Verificar si el contenido es un feed RSS válido
+                        if all(item.find('title') and item.find('link') for item in items):
+                            return "RSS_OK"
+                        else:
+                            return "RSS_OK"
+                    else:
+                        return "RSS_NO_ITEMS"
+                else:
+                    return "XML_CONTENT"
+            else:
+                return "XML_CONTENT"
+
+        # Verificar si el contenido es HTML válido
+        elif content_type.startswith("text/html"):
+            try:
+                soup = BeautifulSoup(content_text, 'html.parser')
+                if soup.find('html'):
+                    return "HTML_CONTENT"
+            except Exception:
+                pass
+
+        # Si no es HTML ni XML/RSS válido, devolver un error
+        return "XML_CONTENT"
+
+    except Exception as e:
+        return "XML_CONTENT"
 
 def analyze_url(url_data):
     try:
         start_time = time.time()
         response = requests.get(url_data['feedUrl'], timeout=10)
-        content_type = response.headers.get("Content-Type", "").lower()
-        error_type = None
+        duration = (time.time() - start_time) * 1000
 
-        if content_type.startswith("text/xml") or content_type.startswith("application/xml"):
-            try:
-                # Usar etree.iterparse para manejar espacios de nombres
-                iterator = etree.iterparse(BytesIO(response.content), events=("start", "end"))
-                _, root = next(iterator)
+        error_type, status_code = check_response_status(response)
+        if not error_type:
+            error_type = analyze_content(response)
 
-                # Configurar el manejo de espacios de nombres
-                for _, element in etree.iterparse(BytesIO(response.content), events=("start-ns",)):
-                    if element:
-                        _, uri = element
-                        etree.register_namespace(element.prefix, uri)
-
-                if root.xpath("//item"):
-                    error_type = None
-                else:
-                    error_type = "XML_NO_ITEM"
-
-                if len(response.content) < 80:
-                    error_type = "XML_INCOMPLETE"
-
-            except etree.XMLSyntaxError:
-                error_type = "XML_SYNTAX_ERROR"
-            except Exception as e:
-                if "Document is empty" in str(e) or len(response.content) < 80:
-                    error_type = "XML_INCOMPLETE"
-                else:
-                    error_type = "NOT_XML"
-        elif content_type.startswith("text/html"):
-            if response.status_code == 404:
-                error_type = "NOT_FOUND"
-            else:
-                error_type = "HTML_CONTENT"
-
-        if "www.limber.io" in url_data['feedUrl'] or "feeds-twitter.limber.io" in url_data['feedUrl']:
-            return None
-
-        duration = (time.time() - start_time) * 1000  # Convertir el tiempo a milisegundos
         result = {
             "_id": url_data["_id"],
             "feedUrl": url_data["feedUrl"],
             "status": url_data["status"],
-            "type": response.status_code,
+            "type": status_code,
             "pageSizeInBytes": len(response.content),
             "durationInfoInMilliseconds": int(duration),
             "error": error_type if error_type else None
         }
-        return result
     except requests.exceptions.RequestException as e:
-        error_code = "REQUEST_ERROR"
-        return {
+            error_code = "REQUEST_NOT_FOUND"
+            result = {
             "_id": url_data["_id"],
             "feedUrl": url_data["feedUrl"],
             "status": url_data["status"],
@@ -71,33 +122,32 @@ def analyze_url(url_data):
             "error": error_code
         }
 
-# Cargar los datos de entrada
-with open("data.json", "r") as f:
-    url_data = json.load(f)
+    return result 
 
-# Analizar cada URL de forma concurrente
-max_workers = 20
-with ThreadPoolExecutor(max_workers=max_workers) as executor:
-    futures = [executor.submit(analyze_url, data) for data in url_data]
-    results = []
-    for index, future in enumerate(as_completed(futures), start=1):
-        try:
+def load_input_data(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
+
+def write_output_data(results, file_path):
+    with open(file_path, "w") as f:
+        json.dump(results, f, indent=2)
+
+def process_urls(url_data):
+    max_workers = 20
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = [executor.submit(analyze_url, data) for data in url_data]
+        results = []
+        for future in as_completed(futures):
             result = future.result()
-            if result is not None:
-                results.append(result)
-            print(f"Analizando URL {index}/{len(url_data)}: {url_data[index-1]['feedUrl']}")
-        except Exception as e:
-            print(f"Error al procesar la URL {index}: {e}")
+            print(f"URL: {result['feedUrl']}, Error: {result['error']}")
+            results.append(result)
 
-# Ordenar los resultados por el campo "type"
-def sort_key(result):
-    try:
-        return int(result["type"])
-    except ValueError:
-        return float('inf')  # Trata los valores no numéricos como infinito
+    return results
 
-results.sort(key=sort_key)
+def main():
+    input_data = load_input_data("data.json")
+    results = process_urls(input_data)
+    write_output_data(results, "output.json")
 
-# Escribir los resultados en un archivo JSON
-with open("output.json", "w") as f:
-    json.dump(results, f, indent=2)
+if __name__ == "__main__":
+    main()
